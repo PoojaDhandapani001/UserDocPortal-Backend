121194bcd625afa42f0aeac227444c4f22de5fc2
PoojaDhandapani <poojadhandapani01@gmail.com>
2026-01-18 01:13:38 +0530

    feat: add duplicate invitation check and update invite link to use environment variable

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,12 +2,6 @@ import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("Cloudinary Config:", process.env.PORT,{
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET ? "[HIDDEN]" : undefined,
-});
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
